66c4135999b9d6e8d8e5995c7d37f231b773445d
Mathias Sondrup <matsondrup@gmail.com>
2016-01-18 22:25:47 +0100

    Added functionality to get back to the game after a finish

--- app/src/main/java/com/example/jonas/galgelegaflevering/GameOverActivity.java
+++ app/src/main/java/com/example/jonas/galgelegaflevering/GameOverActivity.java
@@ -12,12 +12,7 @@ import com.parse.ParseObject;
 
 import java.util.List;
 
-public class GameOverActivity extends Activity implements View.OnClickListener, HighscoreSubscriber {
-
-    TextView gameOverText;
-    Button newGameButton;
-    Button endGameButton;
-    ImageView gameOverImage;
+public class GameOverActivity extends Activity implements HighscoreSubscriber {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,51 +22,19 @@ public class GameOverActivity extends Activity implements View.OnClickListener, 
             getFragmentManager().beginTransaction()
                     .add(R.id.fragmentContainer, new AddToHighscoreFragment())
                     .commit();
+        } else{
+            getFragmentManager().beginTransaction()
+                    .add(R.id.fragmentContainer, new AddToHighscoreFragment())
+                    .commit();
         }
-
-        /*gameOverText = (TextView) findViewById(R.id.gameOverText);
-        newGameButton = (Button) findViewById(R.id.newGameButton);
-        endGameButton = (Button) findViewById(R.id.endGameButton);
-        gameOverImage = (ImageView) findViewById(R.id.gameOverImage);
-*/
-
-
-        /*boolean isGameWon = Galgelogik.getInstance().erSpilletVundet();
-        String word = Galgelogik.getInstance().getOrdet();
-
-        if (isGameWon) {
-
-            gameOverText.setText("Tillyke du vandt! Din score var: " + getScore());
-            gameOverText.setVisibility(View.VISIBLE);
-        } else {
-            gameOverText.setText("Øv du tabte. Ordet var " + word);
-            gameOverText.setVisibility(View.VISIBLE);
-            gameOverImage.setImageResource(R.drawable.forkert6);
-            gameOverImage.setVisibility(View.VISIBLE);
-        }
-
-        newGameButton.setOnClickListener(this);
-        endGameButton.setOnClickListener(this);*/
     }
 
-    @Override
-    public void onClick(View v) {
+    public void returnToGame(boolean playAgain){
         Intent i = new Intent();
-
-        if (v == newGameButton) {
-            i.putExtra("playAgain", true);
-            gameOverText.setVisibility(View.GONE);
-            gameOverImage.setVisibility(View.GONE);
-        } else if (v == endGameButton) {
-            i.putExtra("playAgain", false);
-            gameOverText.setVisibility(View.GONE);
-            gameOverImage.setVisibility(View.GONE);
-        }
-
+        i.putExtra("playAgain", playAgain);
         setResult(Activity.RESULT_OK, i);
         finish();
     }
-
 
     @Override
     public void onHighscoreUpdate(List<ParseObject> highscore) {
--- app/src/main/java/com/example/jonas/galgelegaflevering/GameOverFragment.java
+++ app/src/main/java/com/example/jonas/galgelegaflevering/GameOverFragment.java
@@ -1,11 +1,15 @@
 package com.example.jonas.galgelegaflevering;
 
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -28,6 +32,10 @@ public class GameOverFragment extends Fragment implements View.OnClickListener{
         gameOverStatus = (TextView) layout.findViewById(R.id.gameOverStatus);
         restartGameBtn = (Button) layout.findViewById(R.id.newGameButton);
         changeWordLengthBtn = (Button) layout.findViewById(R.id.changeWordLengthBtn);
+
+        if(!Galgelogik.getInstance().isWordsUpdated())
+            changeWordLengthBtn.setVisibility(View.GONE);
+
         toMainMenuBtn = (Button) layout.findViewById(R.id.toMainMenu);
 
         if(Galgelogik.getInstance().erSpilletVundet()){
@@ -43,11 +51,23 @@ public class GameOverFragment extends Fragment implements View.OnClickListener{
     @Override
     public void onClick(View v) {
         if(v == restartGameBtn){
-
+            ((GameOverActivity) getActivity()).returnToGame(true);
         }else if(v == changeWordLengthBtn){
-
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                if (Galgelogik.getInstance().getPossibleLengths() != null) {
+                    builder.setTitle("Vælg den ønskede ord længde");
+                    builder.setAdapter(new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, Galgelogik.getInstance().getPossibleLengths()),
+                            new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    Galgelogik.getInstance().setWordLength(Galgelogik.getInstance().getPossibleLengths().get(which));
+                                    ((GameOverActivity) getActivity()).returnToGame(true);
+                                }
+                            });
+                    builder.create().show();
+                }
         }else if(v == toMainMenuBtn){
-            
+            ((GameOverActivity) getActivity()).returnToGame(false);
         }
     }
 }
